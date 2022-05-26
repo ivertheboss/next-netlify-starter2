6158793021f7844a36193884c1bd72c92b580ed0
TheColdLemoande <62616414+ivertheboss@users.noreply.github.com>
2022-05-26 11:33:33 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,8 +6,8 @@ export default function Home() {
   return (
     <div className="container">
        <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>😳</text></svg>"></link>
-       <b>This is a test.</b>
-       <input type="text">
+       <b>Code:</b>
+       <input type="text" name="Name" size="20">  
     </div>
   )
 }
