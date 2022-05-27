a4be6f2e450bc4ddc7fd20a1b461558cecd63c0f
TheColdLemoande <62616414+ivertheboss@users.noreply.github.com>
2022-05-27 16:26:32 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -4,11 +4,12 @@ import Footer from '@components/Footer'
 
 export default function Home() {
   return (
-    
+    <title>Awesome interesting stuff</title>
+
     <div>
        <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>👌</text></svg>"></link>
-       <h1>"This is TheColdLemonade's website!"</h1> 
-       <h2>"Welcome!"</h2>
+       <h1>This is TheColdLemonade's website!</h1> 
+       <h2>Welcome!</h2>
     </div>
   )
 }
