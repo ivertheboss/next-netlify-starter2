a73064fbf3e9aed5801ae92e3959a2af668c1e3c
TheColdLemoande <62616414+ivertheboss@users.noreply.github.com>
2022-05-27 16:15:14 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,9 @@ import Footer from '@components/Footer'
 
 export default function Home() {
   return (
-    <title>Hello from TheColdLemonade!</title>
+    <head>
+    <title>Hello from TheColdLemonade!</title>  
+    </head>
     <div style = "position:relative; left:80px; top:2px; background-color:yellow;">
        <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>👌</text></svg>"></link>
        <h1>This is TheColdLemonade's website!</h1> 
