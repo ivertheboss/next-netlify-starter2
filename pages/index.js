87fe5800263de6f36e8c155450d5f373b4d6f247
TheColdLemoande <62616414+ivertheboss@users.noreply.github.com>
2022-05-27 16:32:04 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -2,14 +2,13 @@ import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
 
-<html>
-  <head>
-  <title>Awesome interesting stuff</title>
-  </head>
-</html>
-
 export default function Home() {
   return (
+    <html>
+      <head>
+        <title>Awesome interesting stuff</title>
+      </head> 
+    </html>
     <div>
        <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>👌</text></svg>"></link>
        <h1>This is TheColdLemonade's website!</h1> 
