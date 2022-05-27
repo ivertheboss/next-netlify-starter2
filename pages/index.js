84c147a3d8ff490809a1081e648d2af9ccc0af3b
TheColdLemoande <62616414+ivertheboss@users.noreply.github.com>
2022-05-27 16:44:30 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -7,11 +7,9 @@ export default function Home() {
     <html>
       <head>
         <title>Awesome interesting stuff</title>
+        <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>👌</text></svg>"></link>
+        <h1>Welcome to TheColdLemonades <u>wonderfull</u> website!</h1>  
       </head> 
     </html>
-    <div>
-       <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>👌</text></svg>"></link>
-       <p>Welcome to TheColdLemonades <u>wonderfull</u> website!</p>
-    </div>
   )
 }
