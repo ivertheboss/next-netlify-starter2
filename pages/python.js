7a14c88843cf612352fdf92842aad40406043a05
TheColdLemoande <62616414+ivertheboss@users.noreply.github.com>
2022-05-28 12:20:23 -0400

    Update python.js

--- pages/python.js
+++ pages/python.js
@@ -4,7 +4,9 @@ import Footer from '@components/Footer'
 
 export default function Home() {
   return (
-    <style>
+    <html>
+<head>
+<style>
 .dropdown {
   position: relative;
   display: inline-block;
@@ -24,6 +26,21 @@ export default function Home() {
   display: block;
 }
 </style>
+</head>
+<body>
+
+<h2>Hoverable Dropdown</h2>
+<p>Move the mouse over the text below to open the dropdown content.</p>
+
+<div class="dropdown">
+  <span>Mouse over me</span>
+  <div class="dropdown-content">
+  <p>Hello World!</p>
+  </div>
+</div>
+
+</body>
+</html>
     <html>
       <head>
         <title>Python files</title>
