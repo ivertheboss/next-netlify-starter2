6c845b8c40ca9a3756273d1b1143050aa9e320f0
TheColdLemoande <62616414+ivertheboss@users.noreply.github.com>
2022-05-28 08:11:13 -0400

    Update python.js

--- pages/python.js
+++ pages/python.js
@@ -11,7 +11,7 @@ export default function Home() {
         <title>Python files</title>
         <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22></text>🐍</svg>"></link>
       </head> 
-      <div className="container">
+      <div>
         <h1>Downloads:</h1>  
       </div>
       <div>
