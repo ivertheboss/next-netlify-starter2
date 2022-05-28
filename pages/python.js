de14e64424e008bbed184c7773d042079d0f4dca
TheColdLemoande <62616414+ivertheboss@users.noreply.github.com>
2022-05-28 12:01:45 -0400

    Update python.js

--- pages/python.js
+++ pages/python.js
@@ -9,10 +9,18 @@ export default function Home() {
         <title>Python files</title>
         <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22></text>🐍</svg>"></link>
       </head> 
-      <div>
+      <body>
+        <h1>Python Programs for windows</h1>
         <p>Bulletin Board program</p>
         <a href="BulletinBoard.zip" download> Download</a>
         <p>Tick-tack-toe</p>
+        <a href="tick-tack-toe" download> Download</a>
+      </body>
+      <div>
+        <h1>Python game rescorces</h1>
+        <h2>These files are package installers</h2>
+        <h4>Most games require them</h4>
+        <p>Pygame/ installer</p>
         <a href="tick-tack-toe" download> Download</a>
       </div>
     </html>
