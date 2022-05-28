625cbb1b2038364afc6ab6c53b98ce4084e6a34a
TheColdLemoande <62616414+ivertheboss@users.noreply.github.com>
2022-05-28 18:49:10 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -9,11 +9,11 @@ export default function Home() {
         <title>TheColdLemonade's website</title>
         <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>👌</text></svg>"></link>
       </head> 
-      <div style={{display: 'flex',  justifyContent:'center', alignItems:'center', height: '20vh'}}>
+      <div style={{display: 'flex',  justifyContent:'center', alignItems:'center', height: '40vh'}}>
         <h1>Welcome to TheColdLemonade's <u>wonderfull</u> website!</h1>  
       </div>
-      <div style={{display: 'flex',  justifyContent:'center', alignItems:'center', height: '20vh'}}>
-        <p>This is the link the to python page. It contains most of my python projects. It also has the package downloaders.</p>
+      <div style={{display: 'flex',  justifyContent:'center', alignItems:'center', height: '18vh'}}>
+        <p>This is the link the to python page. It contains most of my python projects. It my custom package downloaders. (only for windows x64) </p>
         <a href="http://www.thecoldlemonade.xyz/python/">
     <input type="button" value="Python page" />
     </a>  
