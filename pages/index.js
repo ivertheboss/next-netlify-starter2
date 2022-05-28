98feb977b90e1d028f275e181fcf5dcb9235e5b7
TheColdLemoande <62616414+ivertheboss@users.noreply.github.com>
2022-05-28 17:10:23 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,15 +6,12 @@ export default function Home() {
   return (
     <html>
       <head>
-        <style>
-div {
-  background-color: lightgrey;
-  width: 300px;
-  border: 15px solid green;
-  padding: 50px;
-  margin: 20px;
-}
-</style>
+        <html>
+Look at me - I'm a yellow box!
+<svg width="20" height="20">
+<rect width="20" height="20" style="fill:#E9E612;stroke-width:3;stroke:rgb(0,0,0)" />
+</svg>
+</html>
         <title>TheColdLemonade's website</title>
         <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>👌</text></svg>"></link>
       </head> 
