25c91815b3ea7ead89185c0dfbfd3e791c464cd6
TheColdLemoande <62616414+ivertheboss@users.noreply.github.com>
2022-05-28 19:55:13 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
+import bgImage from ".lemonade.png";
 
 export default function Home() {
   return (
@@ -9,7 +10,8 @@ export default function Home() {
         <title>TheColdLemonade's website</title>
         <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>👌</text></svg>"></link>
       </head> 
-      <div style={{display: 'flex',  justifyContent:'center', alignItems:'center', height: '40vh', backgroundColor: "#5966b8"}}>
+      <div style={{display: 'flex',  justifyContent:'center', alignItems:'center', height: '40vh', backgroundImage: 'url('+hcbgImage+')',
+          backgroundSize: "cover"}}>
         <h1>Welcome to TheColdLemonade's <u>wonderfull</u> website!</h1>  
       </div>
       <div style={{display: 'flex',  justifyContent:'center', alignItems:'center', height: '18vh'}}>
