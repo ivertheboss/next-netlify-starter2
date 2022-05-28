063fec7944b2946320e9a103dbe63276a8c1f2a2
TheColdLemoande <62616414+ivertheboss@users.noreply.github.com>
2022-05-28 19:16:13 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -4,8 +4,8 @@ import Footer from '@components/Footer'
 
 export default function Home() {
   return (
-    <html>
-      <head style={{backgroundColor: "#6a5acd"}}>
+    <html style={{backgroundColor: "#6a5acd"}}>
+      <head>
         <title>TheColdLemonade's website</title>
         <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>👌</text></svg>"></link>
       </head> 
