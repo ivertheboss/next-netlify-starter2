5edf7ebbd0ceb942e6ed3508bfee99e42428c75d
TheColdLemoande <62616414+ivertheboss@users.noreply.github.com>
2022-05-28 15:13:02 -0400

    Update python.js

--- pages/python.js
+++ pages/python.js
@@ -9,7 +9,9 @@ export default function Home() {
         <title>Python files</title>
         <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22></text>🐍</svg>"></link>
       </head> 
-      <p style="text-align:center">Python downloads</p>
+      <div class="center">
+        <p>Python downloads</p>
+      </div>
       <body>
         <a href="BulletinBoard.zip" download> Test download</a>
       </body>
