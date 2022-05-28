e5a6101b88e8092f491b1428f03e67a04cef4c37
TheColdLemoande <62616414+ivertheboss@users.noreply.github.com>
2022-05-28 12:27:40 -0400

    Update python.js

--- pages/python.js
+++ pages/python.js
@@ -18,13 +18,17 @@ export default function Home() {
       </body>
       <div>
         <h1>Python game rescorces</h1>
-        <div class="dropdown">
-          <span>More info</span>
-          <div class="dropdown-content">
+         <div class="dropdown">
+  <button onclick="myFunction()" class="dropbtn">Dropdown</button>
+  <div id="myDropdown" class="dropdown-content">
+    <a href="#">Link 1</a>
+    <a href="#">Link 2</a>
+    <a href="#">Link 3</a>
+  </div>
+</div> 
             <p>These files are package installers</p>
             <p>Most games require them</p>
-          </div>
-        </div>
+         
         <p>Pygame/ installer</p>
         <a href="tick-tack-toe" download> Download</a>
       </div>
