abb2dd00d7b8d952c6b14757a9dceee21d2b00b2
TheColdLemoande <62616414+ivertheboss@users.noreply.github.com>
2022-05-28 18:25:14 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -10,14 +10,13 @@ export default function Home() {
         <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>👌</text></svg>"></link>
       </head> 
       <div style={{display: 'flex',  justifyContent:'center', alignItems:'center', height: '100vh'}}>
-    <h1> I am centered </h1>
-</div>  
     <div>
         <h1>Welcome to TheColdLemonade's <u>wonderfull</u> website!</h1>  
       </div>
     <a href="http://www.thecoldlemonade.xyz/python/">
     <input type="button" value="Python page" />
     </a>  
+</div>
     </html>
   )
 }
