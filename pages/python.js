ec6f46fa97efaa8097e300bc8d2dd94c20a89f53
TheColdLemoande <62616414+ivertheboss@users.noreply.github.com>
2022-05-28 14:12:05 -0400

    Update python.js

--- pages/python.js
+++ pages/python.js
@@ -7,20 +7,17 @@ export default function Home() {
     <html>
       <head>
         <title>Python files</title>
-        <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22></text>🐍</svg>"></link>
+        <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22>🐍<text y=%22.9em%22 font-size=%2290%22>🐍</text>🐍</svg>">🐍</link>
       </head> 
       <div class="container" style="text-align: center;">
         <h1>Python Programs for windows</h1>
-        <p>Bulletin Board program</p>
-        <a href="BulletinBoard.zip" download> Download</a>
-        <p>Tick-tack-toe</p>
-        <a href="tick-tack-toe" download> Download</a>
+        <a href="BulletinBoard.zip" download> Bulletin Board program</a>
+        <a href="tick-tack-toe" download> Tick-tack-toe</a>
         <h1>Python game rescorces</h1>
-            <p>These files are package installers</p>
-            <p>Most games require them</p>
-         
-        <p>Pygame/ installer</p>
-        <a href="tick-tack-toe" download> Download</a>
+        </div>
+          <p>These files are package installers</p>
+          <p>Most games require them</p>
+        <a href="pygamePackages.py" download> Pygame packages</a>
       </div>
     </html>
   )
