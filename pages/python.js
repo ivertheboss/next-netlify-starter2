c8a18868640dae9320559a0879eecd3f5dd0efac
TheColdLemoande <62616414+ivertheboss@users.noreply.github.com>
2022-05-28 11:16:29 -0400

    Update python.js

--- pages/python.js
+++ pages/python.js
@@ -4,13 +4,14 @@ import Footer from '@components/Footer'
 
 export default function Home() {
   return (
-    <style>
-    </style>
     <html>
       <head>
         <title>Python files</title>
         <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22></text>🐍</svg>"></link>
       </head> 
+      <body>
+        <a href="/files/download-file.pdf" download="newname" >Download Link</a>
+      </body>
     </html>
   )
 }
