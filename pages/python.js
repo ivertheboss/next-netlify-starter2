aa0a0cecdd9780442e7836db67f415296e48b2f6
TheColdLemoande <62616414+ivertheboss@users.noreply.github.com>
2022-05-28 16:54:18 -0400

    Update python.js

--- pages/python.js
+++ pages/python.js
@@ -6,7 +6,6 @@ export default function Home() {
   return (
     <html>
       <head>
-        <div style="width: 300px; height: 300px; background-color: #336699">&nbsp;</div> 
         <title>Python files</title>
         <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22></text>🐍</svg>"></link>
       </head> 
