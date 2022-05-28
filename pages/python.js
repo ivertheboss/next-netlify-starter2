b46b93597cdb9d29c414395197e5c1880736144b
TheColdLemoande <62616414+ivertheboss@users.noreply.github.com>
2022-05-27 22:09:59 -0400

    Update and rename games.js to python.js

---
+++ pages/python.js
@@ -0,0 +1,20 @@
+import Head from 'next/head'
+import Header from '@components/Header'
+import Footer from '@components/Footer'
+
+export default function Home() {
+  return (
+    <html>
+      <head>
+        <title>Python files</title>
+        <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22></text>🐍</svg>"></link>
+      </head> 
+      <div className="container">
+        <h1>Downloads:</h1>  
+      </div>
+      <body>
+         <a href="/cv.pdf" download>BB.py</a>
+      </body>
+    </html>
+  )
+}
