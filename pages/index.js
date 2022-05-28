24bda5c751cf7f4f26ef9d1d142c314692c747c4
TheColdLemoande <62616414+ivertheboss@users.noreply.github.com>
2022-05-28 19:24:39 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -4,12 +4,12 @@ import Footer from '@components/Footer'
 
 export default function Home() {
   return (
-    <html style={{backgroundColor: "#0000cd"}}>
+    <html style={{backgroundColor: "#6872b3"}}>
       <head>
         <title>TheColdLemonade's website</title>
         <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>👌</text></svg>"></link>
       </head> 
-      <div style={{display: 'flex',  justifyContent:'center', alignItems:'center', height: '40vh', backgroundColor: "#4169e1"}}>
+      <div style={{display: 'flex',  justifyContent:'center', alignItems:'center', height: '40vh', backgroundColor: "#5966b8"}}>
         <h1>Welcome to TheColdLemonade's <u>wonderfull</u> website!</h1>  
       </div>
       <div style={{display: 'flex',  justifyContent:'center', alignItems:'center', height: '18vh'}}>
