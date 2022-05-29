93f6fae5b40d7269f0ca67c80c50a52e627faf77
TheColdLemoande <62616414+ivertheboss@users.noreply.github.com>
2022-05-29 12:57:46 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -26,6 +26,7 @@ export default function Home() {
     <input type="button" value="Form page" />
     </a>
    </div>
+      <div style={{display: 'flex',  justifyContent:'center', alignItems:'center', height: '15vh'}}></div>
    <div style={{display: 'flex',  justifyContent:'center', alignItems:'center', height: '35vh', backgroundColor: "#0059ff", borderRadius: '15px'}}>
         <h1>Welcome to TheColdLemonade's <u>wonderfull</u> custom pages!</h1>  
       </div>
