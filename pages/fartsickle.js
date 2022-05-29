128d10251ddfcaae869a3f8a988ce972caf6d530
TheColdLemoande <62616414+ivertheboss@users.noreply.github.com>
2022-05-29 11:47:45 -0400

    Update fartsickle.js

--- pages/fartsickle.js
+++ pages/fartsickle.js
@@ -10,21 +10,20 @@ export default function Home() {
         <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🤥</text></svg>"></link>
       </head> 
       <div style={{display: 'flex',  justifyContent:'center', alignItems:'center', height: '35vh', backgroundColor: "#0059ff", borderRadius: '15px'}}>
-        <h1>Quandale dingle here</h1>
-        <h2>page requested by Grayson George</h2>
+        <h1>Quandale dingle here (page requested by Grayson George)</h1>
       </div>
       <div style={{display: 'flex',  justifyContent:'center', alignItems:'center', height: '15vh'}}></div>
       <div style={{display: 'flex',  justifyContent:'center', alignItems:'center', height: '9vh'}}>
         <iframe width="1189" height="669" src="https://www.youtube.com/embed/fwJo7K-aYEQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
    </div>
-<div style={{display: 'flex',  justifyContent:'center', alignItems:'center', height: '9vh'}}>
+<div style={{display: 'flex',  justifyContent:'center', alignItems:'center', height: '70vh'}}>
         <iframe width="945" height="709" src="https://www.youtube.com/embed/3YHM768tt0c" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>  </div>
-<div style={{display: 'flex',  justifyContent:'center', alignItems:'center', height: '9vh'}}>
+<div style={{display: 'flex',  justifyContent:'center', alignItems:'center', height: '70vh'}}>
         <iframe width="1280" height="720" src="https://www.youtube.com/embed/DdWC239Dbps" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
    </div>
-<div style={{display: 'flex',  justifyContent:'center', alignItems:'center', height: '9vh'}}>
+<div style={{display: 'flex',  justifyContent:'center', alignItems:'center', height: '70vh'}}>
         <iframe width="1189" height="669" src="https://www.youtube.com/embed/xl7y2mMMLD8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>   </div>
-<div style={{display: 'flex',  justifyContent:'center', alignItems:'center', height: '9vh'}}>
+<div style={{display: 'flex',  justifyContent:'center', alignItems:'center', height: '70vh'}}>
         <iframe width="1189" height="669" src="https://www.youtube.com/embed/myL7rMj7Zjg" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>   </div>
    <div style={{display: 'flex',  justifyContent:'center', alignItems:'center', height: '18vh'}}>
     <p>Back to home</p>
