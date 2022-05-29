efe218d32288e2235874bae408203350f987b9ec
TheColdLemoande <62616414+ivertheboss@users.noreply.github.com>
2022-05-28 20:19:58 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ export default function Home() {
         <title>TheColdLemonade's website</title>
         <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>👌</text></svg>"></link>
       </head> 
-      <div style={{display: 'flex',  justifyContent:'center', alignItems:'center', height: '35vh', width: '150', backgroundColor: "#0059ff", borderRadius: '15px'}}>
+      <div style={{display: 'flex',  justifyContent:'center', alignItems:'center', height: '35vh', backgroundColor: "#0059ff", borderRadius: '15px'}}>
         <h1>Welcome to TheColdLemonade's <u>wonderfull</u> website!</h1>  
       </div>
       <div style={{display: 'flex',  justifyContent:'center', alignItems:'center', height: '18vh'}}>
@@ -18,7 +18,11 @@ export default function Home() {
         <a href="http://www.thecoldlemonade.xyz/python/">
     <input type="button" value="Python page" />
     </a>  
-</div>
-    </html>
+   </div>
+   <div style={{display: 'flex',  justifyContent:'center', alignItems:'center'}}>
+    <p>Do you want your own page ohn my site? Fill out the form below.</p>
+    <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdccP9o97w_IMoefAAWZsCX-r9_saYZ_6ffI7xPiv6Ugl_1VQ/viewform?embedded=true" width="800" height="800" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+  </div>    
+  </html>
   )
 }
