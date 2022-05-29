91c8f0d465e7daa648f7afb7a0d80610ba4befb1
TheColdLemoande <62616414+ivertheboss@users.noreply.github.com>
2022-05-28 20:29:15 -0400

    Update customPage.js

--- pages/customPage.js
+++ pages/customPage.js
@@ -1,1 +1,24 @@
-<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdccP9o97w_IMoefAAWZsCX-r9_saYZ_6ffI7xPiv6Ugl_1VQ/viewform?embedded=true" width="640" height="400" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+import Head from 'next/head'
+import Header from '@components/Header'
+import Footer from '@components/Footer'
+
+export default function Home() {
+  return (
+    <html style={{backgroundColor: "#6872b3"}}>
+      <head>
+        <title>Your own page</title>
+        <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>📄</text></svg>"></link>
+      </head> 
+      <div style={{display: 'flex',  justifyContent:'center', alignItems:'center', height: '35vh', backgroundColor: "#0059ff", borderRadius: '15px'}}>
+        <h1>Welcome to TheColdLemonade's <u>wonderfull</u> page form page!</h1>  
+      </div>
+      <div style={{display: 'flex',  justifyContent:'center', alignItems:'center', height: '18vh'}}>
+      <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdccP9o97w_IMoefAAWZsCX-r9_saYZ_6ffI7xPiv6Ugl_1VQ/viewform?embedded=true" width="640" height="400" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+    <p>Back to home</p>
+    <a href="http://www.thecoldlemonade.xyz/">
+    <input type="button" value="Home" />
+   
+   </div>
+  </html>
+  )
+}
