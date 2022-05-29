274679879bbcee869a132c1a70381a74cae704d0
TheColdLemoande <62616414+ivertheboss@users.noreply.github.com>
2022-05-28 20:04:19 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
-import bgImage from ".lemonade.png";
 
 export default function Home() {
   return (
