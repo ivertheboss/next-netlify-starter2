93903ee9d15e8bbd805977afd0f90f345121e5bd
TheColdLemoande <62616414+ivertheboss@users.noreply.github.com>
2022-05-28 20:00:55 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -10,8 +10,7 @@ export default function Home() {
         <title>TheColdLemonade's website</title>
         <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>👌</text></svg>"></link>
       </head> 
-      <div style={{display: 'flex',  justifyContent:'center', alignItems:'center', height: '40vh', backgroundImage: 'url('+hcbgImage+')',
-          backgroundSize: "cover"}}>
+      <div style={{display: 'flex',  justifyContent:'center', alignItems:'center', height: '40vh', backgroundColor: "#0059ff", borderRadius: '10px'}}>
         <h1>Welcome to TheColdLemonade's <u>wonderfull</u> website!</h1>  
       </div>
       <div style={{display: 'flex',  justifyContent:'center', alignItems:'center', height: '18vh'}}>
