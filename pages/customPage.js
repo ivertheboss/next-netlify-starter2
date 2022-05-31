f6715a78ff4b7db95c2763dce3f03cf65d013a7e
TheColdLemoande <62616414+ivertheboss@users.noreply.github.com>
2022-05-31 12:41:28 -0400

    Update customPage.js

--- pages/customPage.js
+++ pages/customPage.js
@@ -4,7 +4,7 @@ import Footer from '@components/Footer'
 
 export default function Home() {
   return (
-    <html style={{backgroundColor: "#6872b3"}}>
+    <html style={{backgroundColor: "#c6c6c6"}}>
       <head>
         <title>Your own page</title>
         <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>📄</text></svg>"></link>
