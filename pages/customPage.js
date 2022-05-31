4c2aca2875e85ebda2a626d3438d2b986db51150
TheColdLemoande <62616414+ivertheboss@users.noreply.github.com>
2022-05-31 19:17:16 -0400

    Update customPage.js

--- pages/customPage.js
+++ pages/customPage.js
@@ -9,7 +9,7 @@ export default function Home() {
         <title>Your own page</title>
         <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>📄</text></svg>"></link>
       </head> 
-      <div style={{display: 'flex',  justifyContent:'center', alignItems:'center', height: '35vh', backgroundColor: "#0059ff", borderRadius: '15px'}}>
+      <div style={{display: 'flex',  justifyContent:'center', alignItems:'center', height: '35vh', backgroundColor: "#ffffff", borderRadius: '15px'}}>
         <h1>Welcome to TheColdLemonade's <u>wonderfull</u> page form page!</h1>  
       </div>
       <div style={{display: 'flex',  justifyContent:'center', alignItems:'center', height: '50vh'}}>
