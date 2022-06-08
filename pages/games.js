e038eea71aa3aeb71310d92607e412b8a4a59b78
TheColdLemoande <62616414+ivertheboss@users.noreply.github.com>
2022-06-08 10:03:41 -0400

    Update games.js

--- pages/games.js
+++ pages/games.js
@@ -9,10 +9,13 @@ export default function Home() {
         <title>Grayson George's page</title>
         <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🎮</text></svg>"></link>
       </head>
-    <p>Madalin Cars</p>
-    <a href="">
+<div>    
+<p>madalin cars multiplayer</p>
+    <a href="https://sites.google.com/view/unblocked-games-mom/madalin-cars-multiplayer">
     <input type="button" value="Site" />
     </a>
+</div>
+  <div>
     <p>Back to home</p>
     <a href="http://www.thecoldlemonade.xyz/">
     <input type="button" value="Home" />
