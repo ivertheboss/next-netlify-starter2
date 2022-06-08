bf532f810a7790517a7dc015d761417b1337aed4
TheColdLemoande <62616414+ivertheboss@users.noreply.github.com>
2022-06-08 09:59:32 -0400

    Create games.js

---
+++ pages/games.js
@@ -0,0 +1,23 @@
+import Head from 'next/head'
+import Header from '@components/Header'
+import Footer from '@components/Footer'
+
+export default function Home() {
+  return (
+    <html style={{backgroundColor: "#c6c6c6"}}>
+      <head>
+        <title>Grayson George's page</title>
+        <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🎮</text></svg>"></link>
+      </head>
+    <p>Madalin Cars</p>
+    <a href="">
+    <input type="button" value="Site" />
+    </a>
+    <p>Back to home</p>
+    <a href="http://www.thecoldlemonade.xyz/">
+    <input type="button" value="Home" />
+    </a>
+   </div>
+  </html>
+  )
+}
