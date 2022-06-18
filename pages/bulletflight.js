c1c1ae801a7d816bfc2760f805b56aa080516f40
TheColdLemoande <62616414+ivertheboss@users.noreply.github.com>
2022-06-18 19:07:35 -0400

    Update bulletflight.js

--- pages/bulletflight.js
+++ pages/bulletflight.js
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <html style={{backgroundColor: "#c6c6c6"}}>
       <head>
-        <title>Your own page</title>
+        <title>Bullet Flight downloader (x64)</title>
         <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22></text></svg>"></link>
       </head> 
       <div style={{display: 'flex',  justifyContent:'center', alignItems:'center', height: '35vh', backgroundColor: "#ffffff", borderRadius: '15px'}}>
